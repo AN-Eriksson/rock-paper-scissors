be9139ab1ad1f3cc2ed5ab88afa3f7da77031ce9
Andreas Eriksson <100693629+AN-Eriksson@users.noreply.github.com>
2024-08-14 10:31:00 +0200

    Add project file structure

---
+++ script.js
@@ -0,0 +1,26 @@
+
+/* Get computer choice */
+
+    /* Function that randomly returns rock, paper or scissors */
+
+
+/* Get human choice */
+
+    /* Function that returns one of the valid choices based on user input */
+
+
+
+/* Keep track of score */
+
+    /* Create two global variables for computer wins and player wins */
+
+
+
+/* Game logic to play one round */
+
+    /* Function that calls for computer and player choices, compares them and declares a winner and
+        increments the correct score variable */
+
+
+
+/* Logic for complete game (5 rounds) */
